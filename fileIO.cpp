16e6bc82f3bd7aada4fcf7c4e6d6da6f9d504e81
Marianne Adams <meadams2@bsu.edu>
2025-02-14 15:39:37 +0000

    Endless loop

--- fileIO.cpp
+++ fileIO.cpp
@@ -2,21 +2,51 @@
 #include <iostream>
 #include <sstream>
 
+void printLine(int, int, std::string);
+
 int main(){
 	std::ifstream inFile;
 	std::string currentLine;
 	std::stringstream converter;
-	std::stringstream ss;
-	int counter;
-	std::string sCounter;
+	
+	std::string sNum1;
+	std::string sNum2;
 	std::string text;
+
+	int num1;
+	int num2;
+	int sum;
 
 	inFile.open("data.csv");
 	while (getline(inFile, currentLine)){
+		std::cout << currentLine << std::endl;
 
-		ss.clear();
-		ss.str("");
+		converter.clear();
+		converter.str(currentLine);
+
+		getline(converter, sNum1, ',');
+		getline(converter, sNum2, ',');
+		getline(converter, text);
+
+		std::cout << sNum1 << std::endl;
+		std::cout << sNum2 << std::endl;
+		std::cout << text << std::endl;
+
+		converter >> sNum1 >> num1;
+		converter >> sNum2 >> num2;
+		
+		printLine(num1, num2, text);
+
 		converter.clear();
 		converter.str("");
+	}//end while loop
+}//end main
 
+void printLine(int num1, int num2, std::string text){
+	int sum = num1 + num2;
+	for (int i = 0; i < sum; i++){
+		std::cout << text;
+	}//end for loop
+	std::cout << std::endl;
+}//end printLine
 
